24400e3bc81607dce2d97266d3033ac90db1d3f0
Rohan Pawar <rohanpawar6112005@gmail.com>
2026-08-26 23:24:29 +0530

    Sulution file is added with code for the problem of 2904

---
+++ Medium/2904. Shortest and Lexicographically Smallest Beautiful String/Solution.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String shortestBeautifulSubstring(String s, int k) {
+        int left = 0;
+        int count = 0;
+        String ans = "";
+
+        for (int right = 0; right < s.length(); right++) {
+
+            if (s.charAt(right) == '1') {
+                count++;
+            }
+
+            while (count > k) {
+                if (s.charAt(left) == '1') {
+                    count--;
+                }
+                left++;
+            }
+
+            if (count == k) {
+
+                while (s.charAt(left) == '0') {
+                    left++;
+                }
+
+                String current = s.substring(left, right + 1);
+
+                if (ans.equals("")
+                        || current.length() < ans.length()
+                        || (current.length() == ans.length()
+                        && current.compareTo(ans) < 0)) {
+                    ans = current;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
