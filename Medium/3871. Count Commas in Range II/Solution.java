d6cb93d7b5dfd2cb1bcf1d45efec1795f7dfc59a
Rohan Pawar <rohanpawar6112005@gmail.com>
2026-09-09 23:53:19 +0530

    Solution file is added for the problem of 3871

---
+++ Medium/3871. Count Commas in Range II/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long countCommas(long n) {
+        long answer = 0;
+
+        for (long start = 1000; start <= n; start *= 1000) {
+            answer += n - start + 1;
+        }
+
+        return answer;
+    }
+}
